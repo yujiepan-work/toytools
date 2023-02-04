ff2d9a635003f0e54da6321043ea944455dc1d65
Pan, Yujie <yujie.pan@intel.com>
2023-02-04 01:07:20 -0800

    add id in product

--- toytools/iterext.py
+++ toytools/iterext.py
@@ -10,5 +10,9 @@ def dict_to_namespace(d: Dict[str, Any]):
 def product(**kwargs):
     keys: List[str] = list(kwargs.keys())
     values = kwargs.values()
+    i = 1
     for item in itertools.product(*values):
-        yield dict_to_namespace(dict(zip(keys, item)))
+        args = dict_to_namespace(dict(zip(keys, item)))
+        args.id = i
+        yield args
+        i += 1
