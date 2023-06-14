09bb85d63219a9e83de444861106b26d57131ea5
Pan, Yujie <yujie.pan@intel.com>
2023-06-14 21:18:55 +0800

    add config product

--- toytools/iterext.py
+++ toytools/iterext.py
@@ -1,6 +1,7 @@
 import itertools
 from argparse import Namespace
-from typing import Iterable, Sequence, Any, List, Dict
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, Iterable, List, Sequence
 
 
 def dict_to_namespace(d: Dict[str, Any]):
@@ -16,3 +17,35 @@ def product(**kwargs):
         args.id = i
         yield args
         i += 1
+
+
+class ConfigProduct:
+    id = -1
+
+    def __init__(self, **kwargs) -> None:
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __iter__(self):
+        yield from [self.__class__(**vars(item)) for item in product(**self._get_class_fields())]
+
+    def __repr__(self) -> str:
+        return self._get_instance_fields().__repr__()
+
+    def _get_class_fields(self):
+        return {k: list(v) for k, v in self.__class__.__dict__.items()
+                if not k.startswith('_') and isinstance(v, Iterable)}
+
+    def _get_instance_fields(self):
+        return {k: v for k, v in self.__dict__.items()
+                if not k.startswith('_')}
+
+
+if __name__ == '__main__':
+    class Config(ConfigProduct):
+        field1 = [1, 2]
+        field2 = 'ab'
+        field3 = (divmod, sum, max)
+
+    for cfg in Config():
+        print(cfg)
